9f8dee81e9f14c934603c2499aeea5ceb847045e
Romain Chardon <romainchardon@outlook.com>
2022-03-20 18:16:19 +0100

    things

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <img v-bind:src="require('/images/pokemon_logo.png')"/>
+</div>
+
+</template>
+
+<script>
+    import { defineComponent } from "vue";
+
+    export default defineComponent({
+        data(){
+            return {}
+        },
+    });
+</script>
